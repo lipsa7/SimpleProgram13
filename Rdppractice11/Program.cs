66f1b2afdab25d8ecbf214e3fa7ca5e96d8aa0f9
lipsa7 <lipsakar98@gmail.com>
2021-08-18 20:38:47 +0530

    Add project files.

---
+++ Rdppractice11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = UserProgramCode.checkcharacters("the picture was great");
+            Console.WriteLine(n);
+
+            Console.ReadKey();
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static int checkcharacters(string str)
+        {
+            int len = str.Length;
+            string str1 = str.Substring(0, 1);
+            string str2 = str.Substring(len - 1);
+            if (str1.Equals(str2))
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+            }
